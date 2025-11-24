3535a08ccc85f42179e570d57d97bb318fd74ac5
mx <11730511+cheng-ruimx@user.noreply.gitee.com>
2025-11-24 22:36:43 +0800

    添加学习vue3目录

---
+++ learn_vue_basic/vue-project/src/01_setup.vue
@@ -0,0 +1,33 @@
+<!-- <script >
+export default {
+    setup() {
+      console.log('setup', this)
+      const message = 'this is message'
+      const logMessage = () => {
+        console.log(message)
+      }
+      return {
+        message,
+        logMessage,
+      }
+    },
+    beforeCreate() {
+      console.log('beforeCreate')
+    },
+  }
+</script> -->
+<script setup>
+  const message = 'this is message'
+  const logMessage = () => {
+    console.log(message)
+  }
+</script>
+<template>
+  <div>
+    <h1>Hello Vue!</h1>
+  </div>
+  <div>
+    {{ message }}
+    <button @click="logMessage">log message</button>
+  </div>
+</template>
---
+++ learn_vue_basic/vue-project/src/02_reactive.vue
@@ -0,0 +1,26 @@
+<script setup>
+// import { reactive } from 'vue'
+
+// const state = reactive({
+//   count: 0,
+// })
+
+// const setCount = () => {
+//   state.count = 0
+// }
+
+import { ref } from 'vue'
+const state = ref(0)
+
+const setCount = () => {
+  // 脚本区域修改reef产生的响应式对象数据必须通过.value属性
+  state.value = 0
+}
+</script>
+
+<template>
+  <div>
+    <button @click="state++">count is: {{ state }}</button>
+    <button @click="setCount">{{ state }}</button>
+  </div>
+</template>
