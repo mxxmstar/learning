d36eca9054477dce6c262701df05e154ef1c26ad
mx <11730511+cheng-ruimx@user.noreply.gitee.com>
2025-11-24 22:36:43 +0800

    添加学习vue3目录

---
+++ learn_vue_basic/vue-project/src/03_computed.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+import { computed } from 'vue'
+
+const list = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+const computedList = computed(() => {
+  return list.value.filter(item => item > 5)
+})
+
+setTimeout(() => {
+  list.value.push(11, 12)
+}, 3000)
+</script>
+
+<template>
+  <div>
+    原始响应数组- {{ list }}
+  </div>
+  <div>
+    计算属性- {{ computedList }}
+  </div>
+</template>
