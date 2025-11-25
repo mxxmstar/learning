3932af7688d64b8c1611ea217d34cd02ab7ae40c
mx <11730511+cheng-ruimx@user.noreply.gitee.com>
2025-11-25 21:14:07 +0800

    20251125临时提交，添加了一下repository中的业务

--- go_project/verify_server/internal/repository/user.go
+++ go_project/verify_server/internal/repository/user.go
@@ -3,8 +3,18 @@ package repository
 import (
 	"context"
 
+	"github.com/mxxmstar/learning/pkg/database"
 	"github.com/mxxmstar/learning/verify_server/internal/domain"
 	"github.com/mxxmstar/learning/verify_server/internal/repository/dao"
+)
+
+// 将 error_converter 中的错误转换为 repository 业务层的错误
+var (
+	// ErrDuplicateEmail 表示邮箱冲突错误
+	ErrDuplicateEmail = database.ErrEmailConflict
+
+	// ErrUserNotFound 表示用户不存在错误
+	ErrUserNotFound = database.ErrUserNotFound
 )
 
 type UserRepository struct {
