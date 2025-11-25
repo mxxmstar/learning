81e6b162e6ea5ba81b5e3becbd258b0554fd3026
mx <11730511+cheng-ruimx@user.noreply.gitee.com>
2025-11-25 21:14:07 +0800

    20251125临时提交，添加了一下repository中的业务

--- go_project/verify_server/internal/service/user.go
+++ go_project/verify_server/internal/service/user.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"context"
 	"errors"
 
+	"github.com/mxxmstar/learning/verify_server/internal/domain"
 	"github.com/mxxmstar/learning/verify_server/internal/repository"
 )
 
@@ -21,3 +23,46 @@ var (
 type UserService struct {
 	repo *repository.UserRepository
 }
+
+func NewUserService(repo *repository.UserRepository) *UserService {
+	return &UserService{repo: repo}
+}
+
+func validateUser(u domain.User) error {
+	if u.Username == "" || u.Email == "" || u.Password == "" {
+		return ErrInvalidUserInfo
+	}
+	// 校验邮箱格式
+	// if !isValidEmail(u.Email) {
+	// 	return ErrInvalidUserInfo
+	// }
+	if len(u.Password) < 6 {
+		return ErrInvalidUserInfo
+	}
+	return nil
+}
+
+// 注册新用户
+func (s *UserService) Signup(ctx context.Context, u domain.User) error {
+	if err := validateUser(u); err != nil {
+		return err
+	}
+	err := s.repo.CreateUser(ctx, &domain.User{
+		Id:       u.Id,
+		Username: u.Username,
+		Email:    u.Email,
+		Password: u.Password, // 先不加密，后续再处理
+		CTime:    u.CTime,
+	})
+
+}
+
+// // 更新用户信息
+// func (s *UserService) UpdateUser(ctx context.Context, userId string, updateInfo map[string]interface{}) error {
+
+// }
+
+// // 删除用户
+// func (s *UserService) DeleteUser(ctx context.Context, userId string) error {
+
+// }
