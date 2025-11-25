847d85fc8ec9a7daf9d00f2c63e53b6be00aa18f
mx <11730511+cheng-ruimx@user.noreply.gitee.com>
2025-11-25 20:29:27 +0800

    解决repository/user.go的冲突

--- go_project/verify_server/internal/service/user.go
+++ go_project/verify_server/internal/service/user.go
@@ -1,9 +1,21 @@
 package service
 
-import "github.com/mxxmstar/learning/verify_server/internal/repository"
+import (
+	"errors"
 
-var ErrDuplicatedKey = repository.ErrDuplicatedKey
-var ErrInvalidUserOrPassword = repository.ErrInvalidUserOrPassword
+	"github.com/mxxmstar/learning/verify_server/internal/repository"
+)
+
+var (
+	// ErrDuplicateEmail 表示邮箱已经被注册使用
+	ErrDuplicateEmail = errors.New("email already registered")
+
+	// ErrInvalidUserInfo 表示用户提交的信息不符合要求
+	ErrInvalidUserInfo = errors.New("invalid user information")
+
+	// ErrUserNotFound 表示未找到指定用户
+	ErrUserNotFound = errors.New("user not found")
+)
 
 // 调用领域对象的方法，实现业务逻辑
 type UserService struct {
