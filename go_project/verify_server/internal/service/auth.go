b5849049b819492af7e7c2f1c2cf1ac64acd0753
mx <11730511+cheng-ruimx@user.noreply.gitee.com>
2025-11-28 17:56:25 +0800

    verify_server打通signup流程

--- go_project/verify_server/internal/service/auth.go
+++ go_project/verify_server/internal/service/auth.go
@@ -1,14 +1,22 @@
 package service
 
 import (
+	"context"
 	"errors"
 
+	"github.com/mxxmstar/learning/verify_server/internal/domain"
 	"github.com/mxxmstar/learning/verify_server/internal/repository"
 )
 
 var (
 	// ErrInvalidCredentials 表示用户名或密码错误
 	ErrInvalidCredentials = errors.New("invalid username or password")
+
+	// ErrUserEmailConflict 表示邮箱冲突错误
+	ErrUserEmailConflict = repository.ErrDuplicateEmail
+
+	// ErrUserUsernameConflict 表示用户名冲突错误
+	ErrUserUsernameConflict = repository.ErrDuplicateUsername
 
 	// ErrUserDisabled 表示用户账户已被禁用
 	ErrUserDisabled = errors.New("user account is disabled")
@@ -25,4 +33,10 @@ func NewAuthService(userRepo *repository.UserRepository) *AuthService {
 	return &AuthService{
 		userRepo: userRepo,
 	}
+}
+
+func (s *AuthService) Signup(ctx context.Context, user *domain.User) error {
+	// 在这里调用 encrypt 对密码进行加密
+
+	return s.userRepo.CreateUser(ctx, user)
 }
