244f3c7c64659ad0e8f7de7146c73a3251c52a68
mxxmstar <2297171005@qq.com>
2025-11-29 17:14:56 +0800

    去除learn_vue_basic文件夹

--- learn_vue_basic/vue-project/src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-// import App from './01_setup.vue'
-// import App from './02_reactive.vue'
-// import App from './03_computed.vue'
-import App from './04_watch.vue'
-// import './assets/main.css'
-createApp(App).mount('#app')
